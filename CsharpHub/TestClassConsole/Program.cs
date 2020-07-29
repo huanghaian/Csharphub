cd15fccd315e78322fd504d8bee95fc8a176bef8
huanghaian <huanghaiang@gmail.com>
2020-07-29 11:55:05 +0800

    code format

--- CsharpHub/TestClassConsole/Program.cs
+++ CsharpHub/TestClassConsole/Program.cs
@@ -59,7 +59,7 @@ namespace TestClassConsole
             };
 
             var testContent = new MultipartFormDataContent();
-            testContent.Add(mixed,"files");
+            testContent.Add(mixed, "files");
             //testContent.Headers.Remove("Content-Type");
             //var reult = testContent.Headers.TryAddWithoutValidation("Content-Type", "multipart/form-data; boundary=" + boundary);
             //testContent.Add(streamContent);
@@ -73,7 +73,7 @@ namespace TestClassConsole
                 Console.WriteLine(res.RequestMessage);
             }
         }
-        public static StreamContent CreateStreamContent(string path,string contentType)
+        public static StreamContent CreateStreamContent(string path, string contentType)
         {
             System.IO.FileStream fileStream = new System.IO.FileStream(path, System.IO.FileMode.Open, System.IO.FileAccess.Read);
             var streamContent = new StreamContent(fileStream);
@@ -84,11 +84,11 @@ namespace TestClassConsole
 
         //public static Func<string, Task<string>> myFunc = async (a) => { await Task.Run(); }
 
-        public static int Test<T1, T2>(Func<T1,T2,int> func,T1 a,T2 t2)
+        public static int Test<T1, T2>(Func<T1, T2, int> func, T1 a, T2 t2)
         {
-            return func(a,t2);
+            return func(a, t2);
         }
-        public static int Add(int a,int b)
+        public static int Add(int a, int b)
         {
             return a + b;
         }
@@ -99,27 +99,27 @@ namespace TestClassConsole
             list.Add(url);
             return list.ToArray();
         }
-        public static Func<T,R> MemoizeThreadSafe<T,R>(Func<T,R> func) where T:IComparable
+        public static Func<T, R> MemoizeThreadSafe<T, R>(Func<T, R> func) where T : IComparable
         {
             ConcurrentDictionary<T, R> cache = new ConcurrentDictionary<T, R>();
-            return arg => cache.GetOrAdd(arg,a=>func(a));
+            return arg => cache.GetOrAdd(arg, a => func(a));
         }
 
         private static void TestTaskDelay()
         {
-            Task.Run(async() =>
+            Task.Run(async () =>
             {
                 Console.WriteLine("开始异步任务");
                 await Task.Delay(2000);
                 Console.WriteLine("异步任务结束");
-                
+
             });
             Console.WriteLine($"{nameof(TestTaskDelay)}");
         }
 
         private static void TestTaskComplatetionSource()
         {
-            Task.Run(async() =>
+            Task.Run(async () =>
             {
                 var tcs = new TaskCompletionSource<int>();
                 new Thread(() =>
@@ -159,7 +159,7 @@ namespace TestClassConsole
         static void GetVal(out int v)
         {
             //out 关键词，传参进来的变量v不需要出事话，但是如果直接对v进行操作会有异常，比如使用v.ToString()【编译器会报错:使用未赋值的变量v】，然后在对V赋值；是不允许的。
-           // v.ToString(); //这里调用ToString(),是不允许的。
+            // v.ToString(); //这里调用ToString(),是不允许的。
             v = 10;
         }
         static void Add(ref int v)
